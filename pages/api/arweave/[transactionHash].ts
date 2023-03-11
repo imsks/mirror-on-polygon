60e93b5f136abe85b2a08f1b7c21ec7d33c7625c
Sachin <sachinkshuklaoo7@gmail.com>
2023-03-12 00:29:32 +0530

    Fetch All Posts

--- pages/api/arweave/[transactionHash].ts
+++ pages/api/arweave/[transactionHash].ts
@@ -15,29 +15,59 @@ export default async function (
     const {transactionHash} = req.query;
 
     // Get Arweave transaction data. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
+    const transaction = (await arweave.transactions.getData(
+      transactionHash as string,
+      {
+        decode: true,
+        string: true,
+      },
+    )) as string;
+
+    const transactionJSON = JSON.parse(transaction);
 
     // Get Arweave transaction status. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
-    const txStatus = undefined;
+    const transactionStatusResponse = await arweave.transactions.getStatus(
+      transactionHash as string,
+    );
+
+    const txStatus =
+      transactionStatusResponse.status === 200 &&
+      transactionStatusResponse.confirmed &&
+      transactionStatusResponse.confirmed?.number_of_confirmations >=
+        MIN_NUMBER_OF_CONFIRMATIONS
+        ? TransactionStatusE.CONFIRMED
+        : TransactionStatusE.NOT_CONFIRMED;
 
     if (txStatus === TransactionStatusE.CONFIRMED) {
-      // Get Arweave transaction block in order to retrieve timestamp. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
-
       // Get Arweave transaction. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
+      const tx = await arweave.transactions.get(transactionHash as string);
 
       // Get Arweave transaction tags. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
+      const tags = {} as PostTagsT;
+      (tx.get('tags') as any).forEach((tag) => {
+        const key = tag.get('name', {decode: true, string: true});
+        tags[key] = tag.get('value', {decode: true, string: true});
+      });
+
+      // Get Arweave transaction block in order to retrieve timestamp. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
+      const block = transactionStatusResponse.confirmed
+        ? await arweave.blocks.get(
+            transactionStatusResponse.confirmed.block_indep_hash,
+          )
+        : null;
 
       // Return JSON response in form:
-      // res.status(200).json({
-      //   id: transactionHash as string,
-      //   data: txData,
-      //   status: txStatus,
-      //   timestamp: block?.timestamp,
-      //   tags,
-      // });
+      res.status(200).json({
+        id: transactionHash as string,
+        data: transactionJSON,
+        status: txStatus,
+        timestamp: block?.timestamp,
+        tags,
+      });
     } else {
       res.status(200).json({
         id: transactionHash as string,
-        data: txData,
+        data: transactionJSON,
         status: txStatus,
       });
     }
